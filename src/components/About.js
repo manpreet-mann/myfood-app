6918ec81a242ea2dd115571fbd6e8ac5ae1a0ded
Manpreet Mann <manpreetmann4788@gmail.com>
2024-06-07 17:00:56 +0530

    updating

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,14 @@ import React from "react";
 function About() {
     return (
         <div className="about">
-            <div>
-                <img src="https://www.w3schools.com/w3images/tablesetting2.jpg"></img>
-            </div>
+        <div className="about-img">
+                <img src="https://www.w3schools.com/w3images/tablesetting2.jpg" />
+                </div>
             <div className="content">
                 <h1>About Catering</h1>
                 <h5>Tradition since 1889</h5>
-                <div className="para">The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.We only use seasonal ingredients.</div>
-                <div className="para2">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                <p className="para">The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.We only use seasonal ingredients.</p>
+                <p className="para2">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
         </div>
     );
